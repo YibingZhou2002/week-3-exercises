bcf59455b38056b5cecaa50e6256e3f04a32fffb
Michael McLeod <michaelmcleod@ucl.ac.uk>
2025-01-08 17:35:30 +0000

    Initial commit

---
+++ Student_Grade_Example/build/source/CMakeFiles/StudentGrades.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentGrades.dir/Student.cpp.o"
+  "CMakeFiles/StudentGrades.dir/Student.cpp.o.d"
+  "libStudentGrades.a"
+  "libStudentGrades.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudentGrades.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Student_Grade_Example/build/test/CMakeFiles/TestGrades.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TestGrades.
+# This may be replaced when dependencies are built.
